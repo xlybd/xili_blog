5a722f2e00c5ba00b2c5344273f7f74d2c0c99bd
xlybd <1535844075@qq.com>
2025-02-21 17:50:12 +0800

    upload project

--- server/flag/enter.go
+++ server/flag/enter.go
@@ -10,6 +10,7 @@ import (
 	"go.uber.org/zap"
 )
 
+// 定义 CLI 标志，用于不同操作的命令行选项
 var (
 	sqlFlag = &cli.BoolFlag{
 		Name:  "sql",
@@ -27,14 +28,31 @@ var (
 		Name:  "es",
 		Usage: "Initializes the Elasticsearch index.",
 	}
+	esExportFlag = &cli.BoolFlag{
+		Name:  "es-export",
+		Usage: "Exports data from Elasticsearch to a specified file.",
+	}
+	esImportFlag = &cli.StringFlag{
+		Name:  "es-import",
+		Usage: "Imports data into Elasticsearch from a specified file.",
+	}
+	adminFlag = &cli.BoolFlag{
+		Name:  "admin",
+		Usage: "Creates an administrator using the name, email and address specified in the config.yaml file.",
+	}
 )
 
+// Run 执行基于命令行标志的相应操作
+// 它处理不同的标志，执行相应操作，并记录成功或错误的消息
 func Run(c *cli.Context) {
+	// 检查是否设置了多个标志
 	if c.NumFlags() > 1 {
 		err := cli.NewExitError("Only one command can be specified", 1)
 		global.Log.Error("Invalid command usage:", zap.Error(err))
 		os.Exit(1)
 	}
+
+	// 根据不同的标志选择执行的操作
 	switch {
 	case c.Bool(sqlFlag.Name):
 		if err := SQL(); err != nil {
@@ -66,12 +84,31 @@ func Run(c *cli.Context) {
 		} else {
 			global.Log.Info("Successfully created ES indices")
 		}
+	case c.Bool(esExportFlag.Name):
+		if err := ElasticsearchExport(); err != nil {
+			global.Log.Error("Failed to export ES data:", zap.Error(err))
+		} else {
+			global.Log.Info("Successfully exported ES data")
+		}
+	case c.IsSet(esImportFlag.Name):
+		if num, err := ElasticsearchImport(c.String(esImportFlag.Name)); err != nil {
+			global.Log.Error("Failed to import ES data:", zap.Error(err))
+		} else {
+			global.Log.Info(fmt.Sprintf("Successfully imported ES data, totaling %d records", num))
+		}
+	case c.Bool(adminFlag.Name):
+		if err := Admin(); err != nil {
+			global.Log.Error("Failed to create an administrator:", zap.Error(err))
+		} else {
+			global.Log.Info("Successfully created an administrator")
+		}
 	default:
 		err := cli.NewExitError("unknown command", 1)
 		global.Log.Error(err.Error(), zap.Error(err))
 	}
 }
 
+// NewApp 创建并配置一个新的 CLI 应用程序，设置标志和默认操作
 func NewApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Go Blog"
@@ -80,11 +117,15 @@ func NewApp() *cli.App {
 		sqlExportFlag,
 		sqlImportFlag,
 		esFlag,
+		esExportFlag,
+		esImportFlag,
+		adminFlag,
 	}
 	app.Action = Run
 	return app
 }
 
+// InitFlag 初始化并运行 CLI 应用程序
 func InitFlag() {
 	if len(os.Args) > 1 {
 		app := NewApp()
@@ -94,7 +135,7 @@ func InitFlag() {
 			os.Exit(1)
 		}
 		if os.Args[1] == "-h" || os.Args[1] == "-help" {
-			fmt.Println("Display help message...")
+			fmt.Println("Displaying help message...")
 		}
 		os.Exit(0)
 	}
--- server/go.mod
+++ server/go.mod
@@ -53,7 +53,8 @@ require (
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 )
