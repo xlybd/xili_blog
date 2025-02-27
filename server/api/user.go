aeefa62468e36758ac7fe11cb9226982fb449ae6
xlybd <1535844075@qq.com>
2025-02-27 16:13:16 +0800

    update project

--- server/api/user.go
+++ server/api/user.go
@@ -18,6 +18,7 @@ import (
 type UserApi struct {
 }
 
+// Register 注册
 func (userApi *UserApi) Register(c *gin.Context) {
 	var req request.Register
 	err := c.ShouldBindJSON(&req)
@@ -27,38 +28,41 @@ func (userApi *UserApi) Register(c *gin.Context) {
 	}
 
 	session := sessions.Default(c)
-
+	// 两次邮箱一致性判断
 	savedEmail := session.Get("email")
-
 	if savedEmail == nil || savedEmail.(string) != req.Email {
 		response.FailWithMessage("This email doesn't match the email to be verified", c)
 		return
 	}
 
-	savedCode := session.Get("code")
+	// 获取会话中存储的邮箱验证码
+	savedCode := session.Get("verification_code")
 	if savedCode == nil || savedCode.(string) != req.VerificationCode {
 		response.FailWithMessage("Invalid verification code", c)
 		return
 	}
 
+	// 判断邮箱验证码是否过期
 	savedTime := session.Get("expire_time")
 	if savedTime.(int64) < time.Now().Unix() {
-		response.FailWithMessage("The verification code has expired, please resent it", c)
+		response.FailWithMessage("The verification code has expired, please resend it", c)
 		return
 	}
 
 	u := database.User{Username: req.Username, Password: req.Password, Email: req.Email}
 
-	_, err = userService.Register(u)
+	user, err := userService.Register(u)
 	if err != nil {
 		global.Log.Error("Failed to register user:", zap.Error(err))
 		response.FailWithMessage("Failed to register user", c)
 		return
 	}
 
-	//userApi.TokenNext(c, user)
+	// 注册成功后，生成 token 并返回
+	userApi.TokenNext(c, user)
 }
 
+// Login 登录接口，根据不同的登录方式调用不同的登录方法
 func (userApi *UserApi) Login(c *gin.Context) {
 	switch c.Query("flag") {
 	case "email":
@@ -70,6 +74,7 @@ func (userApi *UserApi) Login(c *gin.Context) {
 	}
 }
 
+// EmailLogin 邮箱登录
 func (userApi *UserApi) EmailLogin(c *gin.Context) {
 	var req request.Login
 	err := c.ShouldBindJSON(&req)
@@ -78,6 +83,7 @@ func (userApi *UserApi) EmailLogin(c *gin.Context) {
 		return
 	}
 
+	// 校验验证码
 	if store.Verify(req.CaptchaID, req.Captcha, true) {
 		u := database.User{Email: req.Email, Password: req.Password}
 		user, err := userService.EmailLogin(u)
@@ -87,13 +93,14 @@ func (userApi *UserApi) EmailLogin(c *gin.Context) {
 			return
 		}
 
+		// 登录成功后生成 token
 		userApi.TokenNext(c, user)
 		return
 	}
-
 	response.FailWithMessage("Incorrect verification code", c)
 }
 
+// QQLogin QQ登录
 func (userApi *UserApi) QQLogin(c *gin.Context) {
 	code := c.Query("code")
 	if code == "" {
@@ -101,12 +108,14 @@ func (userApi *UserApi) QQLogin(c *gin.Context) {
 		return
 	}
 
+	// 获取访问令牌
 	accessTokenResponse, err := qqService.GetAccessTokenByCode(code)
 	if err != nil {
+		global.Log.Error(err.Error(), zap.Error(err))
 		response.FailWithMessage(err.Error(), c)
-		return
 	}
 
+	// 根据访问令牌进行QQ登录
 	user, err := userService.QQLogin(accessTokenResponse)
 	if err != nil {
 		global.Log.Error("Failed to login:", zap.Error(err))
@@ -114,10 +123,12 @@ func (userApi *UserApi) QQLogin(c *gin.Context) {
 		return
 	}
 
+	// 登录成功后生成 token
 	userApi.TokenNext(c, user)
 }
 
 func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
+	// 检查用户是否被冻结
 	if user.Freeze {
 		response.FailWithMessage("The user is frozen, contact the administrator", c)
 		return
@@ -131,6 +142,7 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 
 	j := utils.NewJWT()
 
+	// 创建访问令牌
 	accessClaims := j.CreateAccessClaims(baseClaims)
 	accessToken, err := j.CreateAccessToken(accessClaims)
 	if err != nil {
@@ -139,6 +151,7 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 		return
 	}
 
+	// 创建刷新令牌
 	refreshClaims := j.CreateRefreshClaims(baseClaims)
 	refreshToken, err := j.CreateRefreshToken(refreshClaims)
 	if err != nil {
@@ -147,7 +160,9 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 		return
 	}
 
+	// 是否开启了多地点登录拦截
 	if !global.Config.System.UseMultipoint {
+		// 设置刷新令牌并返回
 		utils.SetRefreshToken(c, refreshToken, int(refreshClaims.ExpiresAt.Unix()-time.Now().Unix()))
 		c.Set("user_id", user.ID)
 		response.OkWithDetailed(response.Login{
@@ -158,13 +173,16 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 		return
 	}
 
+	// 检查 Redis 中是否已存在该用户的 JWT
 	if jwtStr, err := jwtService.GetRedisJWT(user.UUID); errors.Is(err, redis.Nil) {
+		// 不存在就设置新的
 		if err := jwtService.SetRedisJWT(refreshToken, user.UUID); err != nil {
 			global.Log.Error("Failed to set login status:", zap.Error(err))
 			response.FailWithMessage("Failed to set login status", c)
 			return
 		}
 
+		// 设置刷新令牌并返回
 		utils.SetRefreshToken(c, refreshToken, int(refreshClaims.ExpiresAt.Unix()-time.Now().Unix()))
 		c.Set("user_id", user.ID)
 		response.OkWithDetailed(response.Login{
@@ -173,9 +191,11 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 			AccessTokenExpiresAt: accessClaims.ExpiresAt.Unix() * 1000,
 		}, "Successful login", c)
 	} else if err != nil {
+		// 出现错误处理
 		global.Log.Error("Failed to set login status:", zap.Error(err))
 		response.FailWithMessage("Failed to set login status", c)
 	} else {
+		// Redis 中已存在该用户的 JWT，将旧的 JWT 加入黑名单，并设置新的 token
 		var blacklist database.JwtBlacklist
 		blacklist.Jwt = jwtStr
 		if err := jwtService.JoinInBlacklist(blacklist); err != nil {
@@ -184,12 +204,14 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 			return
 		}
 
+		// 设置新的 JWT 到 Redis
 		if err := jwtService.SetRedisJWT(refreshToken, user.UUID); err != nil {
 			global.Log.Error("Failed to set login status:", zap.Error(err))
 			response.FailWithMessage("Failed to set login status", c)
 			return
 		}
 
+		// 设置刷新令牌并返回
 		utils.SetRefreshToken(c, refreshToken, int(refreshClaims.ExpiresAt.Unix()-time.Now().Unix()))
 		c.Set("user_id", user.ID)
 		response.OkWithDetailed(response.Login{
@@ -200,28 +222,221 @@ func (userApi *UserApi) TokenNext(c *gin.Context, user database.User) {
 	}
 }
 
+// ForgotPassword 找回密码
 func (userApi *UserApi) ForgotPassword(c *gin.Context) {
+	var req request.ForgotPassword
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 
+	session := sessions.Default(c)
+
+	// 两次邮箱一致性判断
+	savedEmail := session.Get("email")
+	if savedEmail == nil || savedEmail.(string) != req.Email {
+		response.FailWithMessage("This email doesn't match the email to be verified", c)
+		return
+	}
+
+	// 获取会话中存储的邮箱验证码
+	savedCode := session.Get("verification_code")
+	if savedCode == nil || savedCode.(string) != req.VerificationCode {
+		response.FailWithMessage("Invalid verification code", c)
+		return
+	}
+
+	// 判断邮箱验证码是否过期
+	savedTime := session.Get("expire_time")
+	if savedTime.(int64) < time.Now().Unix() {
+		response.FailWithMessage("The verification code has expired, please resend it", c)
+		return
+	}
+
+	err = userService.ForgotPassword(req)
+	if err != nil {
+		global.Log.Error("Failed to retrieve the password:", zap.Error(err))
+		response.FailWithMessage("Failed to retrieve the password", c)
+		return
+	}
+	response.OkWithMessage("Successfully retrieved", c)
 }
 
-func (userApi *UserApi) UserCard(c *gin.Context) {}
+// UserCard 获取用户卡片信息
+func (userApi *UserApi) UserCard(c *gin.Context) {
+	var req request.UserCard
+	err := c.ShouldBindQuery(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	userCard, err := userService.UserCard(req)
+	if err != nil {
+		global.Log.Error("Failed to get card:", zap.Error(err))
+		response.FailWithMessage("Failed to get card", c)
+		return
+	}
+	response.OkWithData(userCard, c)
+}
 
-func (userApi *UserApi) Logout(c *gin.Context) {}
+// Logout 登出
+func (userApi *UserApi) Logout(c *gin.Context) {
+	userService.Logout(c)
+	response.OkWithMessage("Successful logout", c)
+}
 
-func (userApi *UserApi) UserResetPassword(c *gin.Context) {}
+// UserResetPassword 修改密码
+func (userApi *UserApi) UserResetPassword(c *gin.Context) {
+	var req request.UserResetPassword
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	req.UserID = utils.GetUserID(c)
+	err = userService.UserResetPassword(req)
+	if err != nil {
+		global.Log.Error("Failed to modify:", zap.Error(err))
+		response.FailWithMessage("Failed to modify, orginal password does not match the current account", c)
+		return
+	}
+	response.OkWithMessage("Successfully changed password, please log in again", c)
+	userService.Logout(c)
+}
 
-func (userApi *UserApi) UserInfo(c *gin.Context) {}
+// UserInfo 获取个人信息
+func (userApi *UserApi) UserInfo(c *gin.Context) {
+	userID := utils.GetUserID(c)
+	user, err := userService.UserInfo(userID)
+	if err != nil {
+		global.Log.Error("Failed to get user information:", zap.Error(err))
+		response.FailWithMessage("Failed to get user information", c)
+		return
+	}
+	response.OkWithData(user, c)
+}
 
-func (userApi *UserApi) UserChangeInfo(c *gin.Context) {}
+// UserChangeInfo 修改个人信息
+func (userApi *UserApi) UserChangeInfo(c *gin.Context) {
+	var req request.UserChangeInfo
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	req.UserID = utils.GetUserID(c)
+	err = userService.UserChangeInfo(req)
+	if err != nil {
+		global.Log.Error("Failed to change user information:", zap.Error(err))
+		response.FailWithMessage("Failed to change user information", c)
+		return
+	}
+	response.OkWithMessage("Successfully changed user information", c)
+}
 
-func (userApi *UserApi) UserWeather(c *gin.Context) {}
+// UserWeather 获取天气
+func (userApi *UserApi) UserWeather(c *gin.Context) {
+	ip := c.ClientIP()
+	weather, err := userService.UserWeather(ip)
+	if err != nil {
+		global.Log.Error("Failed to get user weather", zap.Error(err))
+		response.FailWithMessage("Failed to get user weather", c)
+		return
+	}
+	response.OkWithData(weather, c)
+}
 
-func (userApi *UserApi) UserChart(c *gin.Context) {}
+// UserChart 获取用户图表数据，登录和注册人数
+func (userApi *UserApi) UserChart(c *gin.Context) {
+	var req request.UserChart
+	err := c.ShouldBindQuery(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 
-func (userApi *UserApi) UserList(c *gin.Context) {}
+	data, err := userService.UserChart(req)
+	if err != nil {
+		global.Log.Error("Failed to get user chart:", zap.Error(err))
+		response.FailWithMessage("Failed to user chart", c)
+		return
+	}
+	response.OkWithData(data, c)
+}
 
-func (userApi *UserApi) UserFreeze(c *gin.Context) {}
+// UserList 获取用户列表
+func (userApi *UserApi) UserList(c *gin.Context) {
+	var pageInfo request.UserList
+	err := c.ShouldBindQuery(&pageInfo)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 
-func (userApi *UserApi) UserUnfreeze(c *gin.Context) {}
+	list, total, err := userService.UserList(pageInfo)
+	if err != nil {
+		global.Log.Error("Failed to get user list:", zap.Error(err))
+		response.FailWithMessage("Failed to get user list", c)
+		return
+	}
+	response.OkWithData(response.PageResult{
+		List:  list,
+		Total: total,
+	}, c)
+}
 
-func (userApi *UserApi) UserLoginList(c *gin.Context) {}
+// UserFreeze 冻结用户
+func (userApi *UserApi) UserFreeze(c *gin.Context) {
+	var req request.UserOperation
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	err = userService.UserFreeze(req)
+	if err != nil {
+		global.Log.Error("Failed to freeze user:", zap.Error(err))
+		response.FailWithMessage("Failed to freeze user", c)
+		return
+	}
+	response.OkWithMessage("Successfully freeze user", c)
+}
+
+// UserUnfreeze 解冻用户
+func (userApi *UserApi) UserUnfreeze(c *gin.Context) {
+	var req request.UserOperation
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	err = userService.UserUnfreeze(req)
+	if err != nil {
+		global.Log.Error("Failed to unfreeze user:", zap.Error(err))
+		response.FailWithMessage("Failed to unfreeze user", c)
+		return
+	}
+	response.OkWithMessage("Successfully unfreeze user", c)
+}
+
+// UserLoginList 获取登录日志列表
+func (userApi *UserApi) UserLoginList(c *gin.Context) {
+	var pageInfo request.UserLoginList
+	err := c.ShouldBindQuery(&pageInfo)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+
+	list, total, err := userService.UserLoginList(pageInfo)
+	if err != nil {
+		global.Log.Error("Failed to get user login list:", zap.Error(err))
+		response.FailWithMessage("Failed to get user login list", c)
+		return
+	}
+	response.OkWithData(response.PageResult{
+		List:  list,
+		Total: total,
+	}, c)
+}
