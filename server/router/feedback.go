c88419dac6ad749f6079bcfaa22b4edae19dab93
xlybd <1535844075@qq.com>
2025-03-07 17:15:05 +0800

    upload project

---
+++ server/router/feedback.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"server/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+type FeedbackRouter struct {
+}
+
+func (f *FeedbackRouter) InitFeedbackRouter(Router *gin.RouterGroup, PublicRouter *gin.RouterGroup, AdminRouter *gin.RouterGroup) {
+	feedbackRouter := Router.Group("feedback")
+	feedbackPublicRouter := PublicRouter.Group("feedback")
+	feedbackAdminRouter := AdminRouter.Group("feedback")
+
+	feedbackApi := api.ApiGroupApp.FeedbackApi
+	{
+		feedbackRouter.POST("create", feedbackApi.FeedbackCreate)
+		feedbackRouter.GET("info", feedbackApi.FeedbackInfo)
+	}
+	{
+		feedbackPublicRouter.GET("new", feedbackApi.FeedbackNew)
+	}
+	{
+		feedbackAdminRouter.DELETE("delete", feedbackApi.FeedbackDelete)
+		feedbackAdminRouter.PUT("reply", feedbackApi.FeedbackReply)
+		feedbackAdminRouter.GET("list", feedbackApi.FeedbackList)
+	}
+}
