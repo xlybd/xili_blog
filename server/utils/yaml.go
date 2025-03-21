478028ec98e86feff410af5121b5f5a437f27c41
xlybd <1535844075@qq.com>
2025-03-21 12:35:20 +0800

    upload project

--- server/utils/yaml.go
+++ server/utils/yaml.go
@@ -8,28 +8,28 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const defaultConfigFile = "config.yaml"
+const configFile = "config.yaml"
 
 // 获取配置文件路径，支持环境变量
 func getConfigFilePath() string {
 	if path := os.Getenv("CONFIG_PATH"); path != "" {
 		return path
 	}
-	return defaultConfigFile
+	return configFile
 }
 
 // LoadYAML 从文件中读取 YAML 数据并返回字节数组
 func LoadYAML() ([]byte, error) {
-	configFilePath := getConfigFilePath()
-	return os.ReadFile(configFilePath)
+	configFile := getConfigFilePath()
+	return os.ReadFile(configFile)
 }
 
 // SaveYAML 将全局配置对象保存为 YAML 格式到文件
 func SaveYAML() error {
-	configFilePath := getConfigFilePath()
+	configFile := getConfigFilePath()
 	byteData, err := yaml.Marshal(global.Config)
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(configFilePath, byteData, fs.ModePerm)
+	return os.WriteFile(configFile, byteData, fs.ModePerm)
 }
